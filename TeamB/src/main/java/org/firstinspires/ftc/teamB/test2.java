6e40c10a456daca9ab1bd94b338006773b709724
Hervé <herve.schmitveiler@icloud.com>
2021-01-27 09:45:55 +0800

    here is a test commit

--- TeamB/src/main/java/org/firstinspires/ftc/teamB/test2.java
+++ TeamB/src/main/java/org/firstinspires/ftc/teamB/test2.java
@@ -1,22 +1,35 @@
+//
+// test2
+// test stuff
+//
+// noobmaster69
+// 2021
+//
+
+// beginning of code
+
+// package name
 package org.firstinspires.ftc.teamB;
+
+// import statements
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
-
+// main class
 @TeleOp
-
-public class test2 extends LinearOpMode
-{
+public class test2 extends LinearOpMode { // opening curly bracket
+    // override OpMode
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() { // opening curly bracket
+        // wait for start
         waitForStart();
-        while(opModeIsActive())
-        {
+
+        // while opModeIsActive() is true, repeat
+        while (opModeIsActive()) { // opening curly bracket
             telemetry.addLine("Yeeet2");
             telemetry.update();
-        }
+        } // closing curly bracket ( while (opModeIsActive()) { )
+    } // closing curly bracket ( public void runOpMode() { )
+} // closing curly bracket ( public class test2 extends LinearOpMode { )
 
-    }
-}
+// end of code
